ec44447c22dcf536597ecd8dbbfea082fe805080
billdelvin <billdelvin6@gmail.com>
2020-02-29 15:03:26 +0700

    still fixing landing page

---
+++ application/views/welcome.php
@@ -0,0 +1,54 @@
+<div>
+<!-- navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#home">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#aboutus">About Us</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<!-- carousell -->
+	<div id="home">
+		<div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
+			<div class="carousel-inner">
+				<div class="carousel-item active" data-interval="10000">
+				<img src="./assets/img/buddy_1.png" class="d-block w-100">
+				</div>
+				<div class="carousel-item" data-interval="2000">
+				<img src="./assets/img/buddy_2.png" class="d-block w-100">
+				</div>
+				<div class="carousel-item">
+				<img src="./assets/img/buddy_3.png" class="d-block w-100">
+				</div>
+			</div>
+			<a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div> 
+	</div>
+
+	<!-- container -->	
+	<div class="container">
+		<div class="aboutus" id="aboutus"> 
+			<h1>ABOUT US</h1>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+		</div>
+	</div>
+
+</div>
