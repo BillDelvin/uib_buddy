2cdfabaf9b08554cece4862187e3acfbc43354a5
billdelvin <billdelvin6@gmail.com>
2020-03-06 22:55:18 +0700

    change get data

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -6,12 +6,19 @@ class User extends CI_Controller
     public function index()
     {
         $userData['title'] = 'Dashboard';
-        $userData['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array(); // get from session
+        $userData['users'] = $this->db->get_where('users', ['npmUser' => $this->session->userdata('npmUser')])->row_array(); // get from session
 
         $this->load->view('templates/header', $userData);
         $this->load->view('templates/sidebar');
         $this->load->view('user/index', $userData);
         $this->load->view('templates/footer');
+    }
+
+    public function landingPage(){
+        $title['title'] = "Welcome to Buddy UIB";
+		$this->load->view('templates/index_header', $title);
+		$this->load->view('welcome', );
+		$this->load->view('templates/index_footer');
     }
 
     public function buddyList()
