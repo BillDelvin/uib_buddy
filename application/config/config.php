02c81c23d8f22ec7fec45d9c7ee5f8259daa545b
billdelvin <billdelvin6@gmail.com>
2020-03-06 10:07:32 +0700

    change base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/uib_recruitment_buddy/';
+$config['base_url'] = 'http://localhost/remake_uib_buddy/';
 
 /*
 |--------------------------------------------------------------------------
