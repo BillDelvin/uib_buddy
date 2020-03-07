37e73261a1b6f5c350f4edc4a6ea9ba6586a9c53
billdelvin <billdelvin6@gmail.com>
2020-03-07 20:56:13 +0700

    change router link

--- application/views/admin/sidebar.php
+++ application/views/admin/sidebar.php
@@ -1,23 +1,29 @@
 <!-- Sidebar -->
 <ul class="sidebar navbar-nav">
     <li class="nav-item">
-        <a class="nav-link" href="<?= base_url('user/index'); ?>">
+        <a class="nav-link" href="<?= base_url('admin'); ?>">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span>
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="<?= base_url('user/buddyList'); ?>">
+        <a class="nav-link" href="<?= base_url('admin/eventList'); ?>">
+            <i class="fas fa-calendar-week"></i>
+            <span>List Event</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="<?= base_url('admin/buddyList'); ?>">
             <i class="fas fa-users"></i>
             <span>List Buddy</span>
         </a>
     </li>
-    <li class="nav-item">
+    <!-- <li class="nav-item">
         <a class="nav-link" href="tables.html">
             <i class="fas fa-fw fa-table"></i>
             <span>Note</span>
         </a>
-    </li>
+    </li> -->
 </ul>
 
 <div id="content-wrapper">
