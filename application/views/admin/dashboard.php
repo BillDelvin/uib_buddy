8b945fadd2d96a0c7d04b23a067367f5320b1bc0
billdelvin <billdelvin6@gmail.com>
2020-03-07 20:54:02 +0700

    change admin dashboard

---
+++ application/views/admin/dashboard.php
@@ -0,0 +1,9 @@
+<div class="container-fluid">
+    <div class="card" style="width: 20rem;">
+        <img src="<?= base_url('assets/'); ?>img/logo_uib_2.png" class="card-img-top">
+        <div class="card-body">
+            <h5 class="card-title">Profil</h5>
+            <p class="card-text">Name : <?= $user['nameMahasiswa']; ?></p>
+        </div>
+    </div>
+</div>
