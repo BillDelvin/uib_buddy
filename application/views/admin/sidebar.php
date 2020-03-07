98be3b20cf3266b5f444b763fcac57548c7d2d8c
billdelvin <billdelvin6@gmail.com>
2020-03-07 15:48:32 +0700

    move folder

---
+++ application/views/admin/sidebar.php
@@ -0,0 +1,23 @@
+<!-- Sidebar -->
+<ul class="sidebar navbar-nav">
+    <li class="nav-item">
+        <a class="nav-link" href="<?= base_url('user/index'); ?>">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="<?= base_url('user/buddyList'); ?>">
+            <i class="fas fa-users"></i>
+            <span>List Buddy</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="tables.html">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Note</span>
+        </a>
+    </li>
+</ul>
+
+<div id="content-wrapper">
