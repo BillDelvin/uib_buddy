e1fa1d363b6ab421b531a0a5de0674d75d505f43
billdelvin <billdelvin6@gmail.com>
2020-03-08 21:42:15 +0700

    adding buddy list

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -112,18 +112,18 @@ class Admin extends CI_Controller
         redirect('admin/eventList');
     }
 
+    public function buddyList()
+    {
+        $userData['title'] = "Buddy List";
+        $userData['user'] = $this->session->userdata();
+        // $userData['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        // $result = $this->db->get('buddy')->result();
+        // $buddyData["buddy"] = json_decode(json_encode($result), true);
+    
+        $this->load->view('templates/header', $userData);
+        $this->load->view('templates/sidebar');
+        $this->load->view('admin/buddy_list', $userData);
+        $this->load->view('templates/footer');
+    }
 }
 
-
-// public function buddyList()
-// {
-//     $userData['title'] = "Buddy List";
-//     $userData['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
-//     $result = $this->db->get('buddy')->result();
-//     $buddyData["buddy"] = json_decode(json_encode($result), true);
-
-//     $this->load->view('templates/header', $userData);
-//     $this->load->view('templates/sidebar');
-//     $this->load->view('user/buddy_list', $buddyData);
-//     $this->load->view('templates/footer');
-// }
