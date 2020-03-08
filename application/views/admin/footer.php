33736bddbe851e3bcbd53f6c9479e7d6428f2e48
billdelvin <billdelvin6@gmail.com>
2020-03-08 19:13:36 +0700

    adding data table

--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -35,8 +35,20 @@
 <!-- Core plugin JavaScript-->
 <script src="<?= base_url('assets/'); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
 
+<!-- datatable -->
+<script src="<?= base_url('assets/'); ?>js/datatables.min.js"></script>
+
 <!-- Custom scripts for all pages-->
 <script src="<?= base_url('assets/'); ?>js/sb-admin.min.js"></script>
+
+<!-- script -->
+<script>
+    $(document).ready(function() {
+        $('#example').DataTable( {
+            "pagingType": "full_numbers"
+        } );
+    });
+</script>
 </body>
 
 </html>
