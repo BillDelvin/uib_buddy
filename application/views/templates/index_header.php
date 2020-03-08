5ff33cc2762840f602f9b5c885620cda7efe19fa
billdelvin <billdelvin6@gmail.com>
2020-03-09 00:50:15 +0700

    fixing url

--- application/views/templates/index_header.php
+++ application/views/templates/index_header.php
@@ -12,12 +12,12 @@
     <title><?= $title; ?></title>
     
     <!-- Custom fonts for this template-->
-    <link href="./assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="<?= base_url('assets/') ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 
     <!-- bootstrap -->
-    <link href="./assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?= base_url('assets/') ?>css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="./assets/css/style.css" rel="stylesheet">
+    <link href="<?= base_url('assets/') ?>css/style.css" rel="stylesheet">
 
 </head>
