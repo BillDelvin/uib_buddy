2a2b78f0df5fab9a5d8a1128c56c0b5619a13647
billdelvin <billdelvin6@gmail.com>
2020-03-09 23:46:52 +0700

    adding event view

--- application/views/user/event.php
+++ application/views/user/event.php
@@ -1,13 +1,64 @@
-<div class="container-fluid">
-    <div class="card">
-        <div class="card-header">
-            Featured
+<!-- navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  	<a class="navbar-brand" href="#">
+  		<img src="<?= base_url('assets/	'); ?>img/logo_UIB_White.png" width="40" alt="">
+  	</a>
+  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    	<span class="navbar-toggler-icon"></span>
+  	</button>
+
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item active">
+			<a class="nav-link font-weight-bold" href="<?= base_url('welcome') ?>">Home <span class="sr-only">(current)</span></a>
+			</li>
+			<li class="nav-item">
+			<a class="nav-link font-weight-bold" href="<?= site_url('welcome') ?>">About Us</a>
+			</li>
+			<li class="nav-item">
+			<a class="nav-link font-weight-bold" href="<?= base_url('user/event'); ?>">Event</a>
+			</li>
+		</ul>
+		<form class="form-inline my-2 my-lg-0">
+			<?php if(!isset($_SESSION['npmUser'])) : ?>
+				<a href="<?= base_url('auth/sign_in'); ?>" class="btn btn-outline-light my-2 my-sm-0" type="submit">Sign in</a>
+			<?php else : ?>
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<?= $user['nameMahasiswa'] ." ". $user['npmUser']; ?>
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<?php if($user['role'] == 1) : ?>
+								<a class="dropdown-item" href="<?= base_url('admin'); ?>">Dashboard</a>
+							<?php endif; ?>
+							<a class="dropdown-item" href="<?= base_url('auth/logout'); ?>">Log Out</a>
+						</div>
+					</li>
+				</ul>
+			<?php endif; ?>
+		</form>
+	</div>
+</nav>
+
+<div class="container pt-3">
+    <h3 class="text-center">EVENT</h3>
+    <?php foreach ($event as $e ) { ?>
+        <div class="pb-3">
+            <div class="card">
+                <div class="card-header">
+                    <h5><?= $e["eventTitle"]; ?></h5>
+                </div>
+                <div class="card-body">
+                    <p class="card-text"><?= $e["eventDescription"]; ?></p>
+					<?php if(!isset($_SESSION['npmUser'])) : ?>
+						<a href="<?= base_url('auth/sign_in') ?>" class="btn btn-primary">Register</a>
+					<?php else : ?>
+                    	<a href="<?= base_url('user/buddyRegisterForm') ?>" class="btn btn-primary">Register</a>
+					<?php endif; ?>
+                </div>
+            </div>
         </div>
-        <div class="card-body">
-            <h5 class="card-title">Special title treatment</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-    </div>
+    <?php } ?>
 </div>
 <!-- /.container-fluid -->
