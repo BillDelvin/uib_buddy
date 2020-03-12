cd5b2e9c4ae19034b491a6de63ea4c46797ee451
billdelvin <billdelvin6@gmail.com>
2020-03-12 16:28:49 +0700

    adding event

---
+++ application/views/admin/event.php
@@ -0,0 +1,29 @@
+<div class="container-fluid">
+    <h3 class="text-center">EVENT</h3>
+    <div class="mt-3">
+        <table id="myTable" class="display" style="width:100%">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Event Name</th>
+                    <th>Event Description</th>
+                    <th>Member</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php $i = 1; ?>
+                <?php foreach ($eventBuddy as $event ) { ?>
+                    <tr>
+                        <td data-label="No"><?= $i; ?></td>
+                        <td data-label="Event Name"><?= $event["eventTitle"]; ?></td>
+                        <td data-label="Event Description"><?= $event["eventDescription"]; ?></td>
+                        <td data-label="Member" class="text-center">  
+                            <a href="<?= site_url('admin/eventMember/'.$event['idEvent']); ?>" class="btn btn-outline-secondary" value="<?= $event['idEvent']; ?>"><i class="fas fa-users"></i></a> 
+                        </td>
+                    </tr>
+                <?php $i++; ?>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+</div>
