7237baaffe322dcaa18c4f146fcd5754e59a7937
billdelvin <billdelvin6@gmail.com>
2020-03-15 21:05:23 +0700

    fixing data tabel jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,3 @@
-$(document).ready(function() {
-    $('#example').DataTable( {
-        "pagingType": "full_numbers"
-    } );
-    
-    $('#myTable').DataTable().columns.adjust();
-});
+$(document).ready( function () {
+    $('#myTabel').DataTable();
+} );
