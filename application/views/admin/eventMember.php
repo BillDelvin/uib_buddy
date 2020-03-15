f6e962d112616d50612ded83e900461eca3ce744
billdelvin <billdelvin6@gmail.com>
2020-03-15 22:05:12 +0700

    change data label name

--- application/views/admin/eventMember.php
+++ application/views/admin/eventMember.php
@@ -2,38 +2,38 @@
     <div class="text-left pb-3">
         <a href="<?= base_url('admin/event'); ?>"><i class="fas fa-3x fa-arrow-left"></i></a>
     </div>
-    <table id="example" class="display compact" style="width:100%">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>NPM</th>
-                <th>Name</th>
-                <th>Photo</th>
-                <th>Email</th>
-                <th>Phone Number</th>
-                <th>Url Video</th>
-                <th>Status</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php $i = 1; ?>
-            <?php foreach ($buddy as $b ) { ?>
+    <div class="table-responsive"> 
+        <table id="myTabel" class="display" style="width:100%">
+            <thead>
                 <tr>
-                    <td data-label="No"><?= $i; ?></td>
-                    <td data-label="Event Name"><?= $b["npmUser"]; ?></td>
-                    <td data-label="Event Description"><?= $b["nameMahasiswa"]; ?></td>
-                    <td data-label="Event Description"><?= $b["image"]; ?></td>
-                    <td data-label="Event Description"><?= $b["email"]; ?></td>
-                    <td data-label="Event Description"><?= $b["noPhoneMahasiswa"]; ?></td>
-                    <td data-label="Event Description"> <a href=""><?= $b["urlVideo"]; ?></a> </td>
-                    <td data-label="Event Description"><?= $b["status"]; ?></td>
-                    <td data-label="Action"> 
-                        <a href="<?= site_url('admin/eventMember/'.$b['npmUser']); ?>" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></a>
-                    </td>
+                    <th>NPM</th>
+                    <th>Name</th>
+                    <th>Photo</th>
+                    <th>Email</th>
+                    <th>Phone Number</th>
+                    <th>Url Video</th>
+                    <th>Status</th>
+                    <th>Action</th>
                 </tr>
-            <?php $i++; ?>
-            <?php } ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php foreach ($buddy as $b ) { ?>
+                    <tr>
+                        <td data-label="NPM"><?= $b["npmUser"]; ?></td>
+                        <td data-label="Name"><?= $b["nameMahasiswa"]; ?></td>
+                        <td data-label="Photo">
+                            <img width="100px" src="<?= base_url('assets/'); ?>buddy_img/<?= $b["image"]; ?>">
+                        </td>
+                        <td data-label="Email"><?= $b["email"]; ?></td>
+                        <td data-label="Phone Number"><?= $b["noPhoneMahasiswa"]; ?></td>
+                        <td data-label="Url Video"> <a href="<?= $b["urlVideo"]; ?>"><?= $b["urlVideo"]; ?></a> </td>
+                        <td data-label="Status"><?= $b["status"]; ?></td>
+                        <td data-label="Action"> 
+                            <a href="<?= site_url('admin/eventMember/'.$b['npmUser']); ?>" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></a>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
 </div>
