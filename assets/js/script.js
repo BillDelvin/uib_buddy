be9171455ddf9fb22cbc420c80dcb00b6264b8d0
billdelvin <billdelvin6@gmail.com>
2020-03-19 23:01:00 +0700

    adding responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
 $(document).ready( function () {
-    $('#myTabel').DataTable();
+    $('#myTable').DataTable( {
+        responsive: true
+    } );
 } );
+
+window.setMobileTable = function(selector) {
+    // if (window.innerWidth > 600) return false;
+    const tableEl = document.querySelector(selector);
+    const thEls = tableEl.querySelectorAll('thead th');
+    const tdLabels = Array.from(thEls).map(el => el.innerText);
+    tableEl.querySelectorAll('tbody tr').forEach( tr => {
+      Array.from(tr.children).forEach( 
+        (td, ndx) =>  td.setAttribute('label', tdLabels[ndx])
+      );
+    });
+  }
