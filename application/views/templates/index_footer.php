ff66e9d1f758744290a1237fe43306864ada5526
billdelvin <billdelvin6@gmail.com>
2020-03-21 20:58:13 +0700

    adding data table script

--- application/views/templates/index_footer.php
+++ application/views/templates/index_footer.php
@@ -5,6 +5,12 @@
 <!-- Core plugin JavaScript-->
 <script src="<?= base_url('assets/'); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
 
+<!-- Custom scripts for all pages-->
+<script src="<?= base_url('assets/'); ?>vendor/datatables/jquery.dataTables.min.js"></script>
+
+<!-- script -->
+<script src="<?= base_url('assets/'); ?>js/script.js"></script>
+
 <!-- custom script -->
 <script>
     $('.custom-file-input').on('change', function() {
