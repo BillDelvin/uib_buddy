93a829f06055eceb0b19dc64a55098dcf4311fa4
billdelvin <billdelvin6@gmail.com>
2020-03-29 21:44:04 +0700

    adding button interview

--- application/views/user/yourEvent.php
+++ application/views/user/yourEvent.php
@@ -61,7 +61,15 @@
                         <td data-label="No"><?= $i; ?></td>
                         <td data-label="Event Name"><?= $ue["eventTitle"]; ?></td>
                         <td data-label="Event Date"><?= $ue["eventDate"]; ?></td>
-                        <td data-label="Email"><?= $ue['status']; ?></td>
+                        <td data-label="Email">
+                            <?php if( $ue['status'] == 'interview' ) :?>
+                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                                    <?= $ue['status']; ?>
+                                </button>
+                            <?php else : ?>
+                                <?= $ue['status']; ?>
+                            <?php endif; ?>
+                        </td>
                     </tr>
                 <?php $i++; ?>
                 <?php } ?>
@@ -69,4 +77,24 @@
             </table>
         </div>
     </div>
+</div>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        interview process
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
 </div>
