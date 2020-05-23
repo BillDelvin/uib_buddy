2c7303a88eb3f9150278620af48d1977c9e651a6
billdelvin <billdelvin6@gmail.com>
2020-05-23 18:24:48 +0700

    still process adding phpmailer

---
+++ application/libraries/Phpmailer_lib.php
@@ -0,0 +1,31 @@
+<?php 
+defined("BASEPATH") OR exit("No direct script access allowed");
+
+/**
+ * CodeIgniter PMPMailer class
+ * 
+ * This class enables SMTP email with PHPMailer
+ * 
+ */
+
+
+ use PHPMailer\PHPMailer\PHPMailer;
+ use PHPMailer\PHPMailer\Exception;
+ class PHPMailer_Lib
+ {
+    public function __contruct()
+    {
+        log_message("Debug","PHPMailer class is loaded");
+
+    }
+
+    public function load()
+    {
+        require_once APPPATH."third_party/PHPMailer/Exception.php";
+        require_once APPPATH."third_party/PHPMailer/PHPMailer.php";
+        require_once APPPATH."third_party/PHPMailer/SMTP.php";
+
+        $email = new PHPMailer;
+        return $email;
+    }
+ }
