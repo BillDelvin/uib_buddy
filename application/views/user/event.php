d997eff7b1aaeb36a507b51586ee497701db5f1f
billdelvin <billdelvin6@gmail.com>
2020-05-29 14:08:29 +0700

    fixing navigate

--- application/views/user/event.php
+++ application/views/user/event.php
@@ -13,7 +13,7 @@
 				<a class="nav-link font-weight-bold" href="<?= base_url('welcome') ?>">Home <span class="sr-only">(current)</span></a>
 			</li>
 			<li class="nav-item">
-				<a class="nav-link font-weight-bold" href="<?= base_url('user/event'); ?>">Event</a>
+				<a class="nav-link font-weight-bold" href="<?= base_url('user/buddyEvent'); ?>">Event</a>
 			</li>
 		</ul>
 		<form class="form-inline my-2 my-lg-0">
