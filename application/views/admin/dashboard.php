6706dc494ddcdf6ee1f9f92d72cd9a0c263a6f33
billdelvin <billdelvin6@gmail.com>
2020-06-03 22:58:41 +0700

    change admin dashboard

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,9 +1,13 @@
 <div class="container-fluid">
-    <div class="card" style="width: 20rem;">
-        <img src="<?= base_url('assets/'); ?>img/logo_uib_2.png" class="card-img-top">
-        <div class="card-body">
-            <h5 class="card-title">Profil</h5>
-            <p class="card-text">Name : <?= $user['nameMahasiswa']; ?></p>
+    <div class="text-center">
+        <h1>Welcome To E-Recruitment Buddy</h1>
+        <hr>
+        <div class="card" style="width: 20rem;">
+            <img src="<?= base_url('assets/'); ?>img/logo_uib_2.png" class="card-img-top">
+            <div class="card-body text-left">
+                <h5 class="card-title">Profil</h5>
+                <p class="card-text">Name : <?= $user['nameMahasiswa']; ?></p>
+            </div>
         </div>
     </div>
 </div>
